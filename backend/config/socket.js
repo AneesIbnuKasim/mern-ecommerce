8e051e04e830402eea16e02f5a7f9cb9e442d628
Anees <Aneesdropz@gmail.com>
2025-06-06 20:07:24 +0530

    set up Socket.io on backend with auth

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -1,4 +1,6 @@
+import jwt  from "jsonwebtoken";
 import { Server } from "socket.io";
+import messageModel from "../models/messageModel";
 
 let io;
 
@@ -9,14 +11,37 @@ const setupSocket = (server) => {
       methods: ["GET", "POST"],
     }, 
   });
-
+  io.use((socket, next)=>{
+    const token = socket.handshake.auth?.token;
+    if (!token) return next( new Error("Authentciation Error: You are not logged in"));
+    try {
+      const user = jwt.verify(token,process.env.jwt_secret);
+      socket.user = user;
+      next();
+    } catch (error) {
+      return next( new Error("Authentcation Error: Invalid Token"));
+    }
+  })
+  const userSocketMap = {}
   io.on("connection", socket => {
-    console.log("User connected:", socket.id);
-
-    socket.on("sendMessage", (data) => {
-      console.log("Message received:", data);
-      io.emit("receiveMessage", data); // broadcast to all
+    console.log("User connected:", socket.id, socket.user);
+    const userId = socket.user.id;
+    userSocketMap[userId] = socket.id;
+    console.log(`user connected: ${userId}`);
+   
+    
+    socket.on("join room", () => {
+      const roomId = `room-${userId}`
+      socket.emit("joined room",roomId); // broadcast to user
     });
+    socket.on('send message',async({ roomId, message , toUserId })=>{
+      const newMessage = new messageModel({
+        formUserId : userId,
+        toUSerId : toUserId,
+        message : message
+      })
+      await newMessage.save();
+    })
 
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
---
+++ backend/models/messageModel.js
@@ -0,0 +1,10 @@
+import mongoose  from "mongoose";
+const messageSchema = new mongoose.Schema({
+    messageId : {type: 'String', required: true},
+    fromUserId : {type: 'String', reuired: true},
+    toUserId : {type: 'String', reuired: true},
+    message : {type : 'String', required: true},
+    timestamp : {type : Date, default: Date.now}
+})
+const messageModel = mongoose.models.message || mongoose.model('message',messageSchema);
+export default messageModel
